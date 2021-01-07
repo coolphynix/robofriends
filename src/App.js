21bfc34fe8af27b2d76aee8e6182ad5c5be79bec
coolphynix <sandeepsen09@gmail.com>
2021-01-07 17:27:40 +0530

    Second Change

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Component} from 'react';
 import CardList from './CardList'
-import { robots } from './robots';
 import SearchBox from './SearchBox';
 import './App.css';
+import Scroll from './Scroll';
 
 
 class App extends Component {
 	constructor() {
 		super()
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchfield: ''
 		}
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => response.json())
+		.then(users => {this.setState({ robots: users})});
 	}
 
 	onSearchange = (event) => {
@@ -22,17 +28,23 @@ class App extends Component {
 		const filteredRobots = this.state.robots.filter(robot => {
 		return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
 		})
-		return (
+
+		if(this.state.robots.length === 0){
+			return <h1>Loading.....</h1>
+		} else {
+			return (
 			<div className='tc'>
 				<h1>Robofriends</h1>
 				<SearchBox searchChange = {this.onSearchange}/>
-				<CardList robots={filteredRobots}/>
+				<Scroll>
+					<CardList robots={filteredRobots}/>
+				</Scroll>
 			</div>
-
 		);
+		}
+		
 	}
 }
 
-	
 
 export default App;
